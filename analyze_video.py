78b928d00c5214b4d8275d37cf52aeb50c9fb576
serge133 <53378287+serge133@users.noreply.github.com>
2022-12-09 14:12:57 -0800

    Optimized DFS, removed useless files, faster motion detection

--- analyze_video.py
+++ analyze_video.py
@@ -2,7 +2,6 @@ import cv2
 import numpy as np
 from collections import deque 
 import os
-# from discord_api import send_message
 
 """HOW MUCH OF THE PIXELS SHOULD CHANGE TO BE CONSIDERED CHANGED"""
 # THRESHOLD = 25
@@ -22,48 +21,80 @@ GRID = 16
 # make smaller if you want to detect small objects 
 DFS_SKIP = 2
 
-def outOfBound(matrix, pixel_x, pixel_y):
-    return (pixel_x > len(matrix[0]) - 1 or pixel_y > len(matrix) - 1 or pixel_x < 0 or pixel_y < 0)
+COLOR: tuple[int, int, int] = (0, 0, 255) # RED
 
-def dfs(matrix, x, y, visited, og, frame_num):
-    if outOfBound(matrix, x, y) or matrix[y][x] < THRESHOLD or f"{x},{y}" in visited:
-        return 0 
+# Checks if a pixel is out of bounds of the image 
+# def outOfBound(matrix, pixel_x, pixel_y):
+#   return (pixel_x > len(matrix[0]) - 1 or pixel_y > len(matrix) - 1 or pixel_x < 0 or pixel_y < 0)
 
-    queue = deque()
-    queue.append([x, y])
+def isChangedPixel(matrix, coordinate: tuple[int, int], visited: set) -> bool:
+    # Out of bound
+    if (coordinate[0] > len(matrix[0]) - 1 or coordinate[1] > len(matrix) - 1 or coordinate[0] < 0 or coordinate[1] < 0):
+        return False
+    # Already Visited
+    if coordinate in visited:
+        return False
+    # Does not meet threshold of change to be valid changed pixel
+    if matrix[coordinate[1]][coordinate[0]] < THRESHOLD:
+        return False 
+    return True
+
+# Performs a depth first search of all the changed pixels in the area 
+def dfs(matrix: cv2.Mat, x: int, y: int, visited: set[tuple[int, int]], original_image, frame_number: int):
+    if not isChangedPixel(matrix, (x, y), visited):
+        return 0
+
+    queue: deque[tuple[int, int]] = deque([(x, y)])
     count: int = 0
     
     # For drawing A rectangle
     top_left = [float('inf'), float('inf')]
     bottom_right = [0, 0]
 
-
-
     while queue:
         pixel_x, pixel_y = queue.popleft()
 
-        if outOfBound(matrix, pixel_x, pixel_y) or matrix[pixel_y][pixel_x] < THRESHOLD or f"{pixel_x},{pixel_y}" in visited:
-            continue
+        # if outOfBound(matrix, pixel_x, pixel_y) or matrix[pixel_y][pixel_x] < THRESHOLD or f"{pixel_x},{pixel_y}" in visited:
+            # continue
 
         top_left = [min(top_left[0], pixel_x), min(top_left[1], pixel_y)]
         bottom_right = [max(bottom_right[0], pixel_x), max(bottom_right[1], pixel_y)]
-        og[pixel_y][pixel_x] = [0, 0, 255]
-        count += 1
-        visited.add(f"{pixel_x},{pixel_y}")
 
-        queue.append([pixel_x + DFS_SKIP, pixel_y])
-        queue.append([pixel_x - DFS_SKIP, pixel_y])
-        queue.append([pixel_x, pixel_y + DFS_SKIP])
-        queue.append([pixel_x, pixel_y - DFS_SKIP])
+        original_image[pixel_y][pixel_x] = COLOR
+        count += 1
+        visited.add((pixel_x, pixel_y))
+
+        right = (pixel_x + DFS_SKIP, pixel_y)
+        left = (pixel_x - DFS_SKIP, pixel_y)
+        top = (pixel_x, pixel_y + DFS_SKIP)
+        bottom = (pixel_x, pixel_y - DFS_SKIP)
+
+        if isChangedPixel(matrix, right, visited):
+            visited.add(right)
+            queue.append(right)
+        if isChangedPixel(matrix, left, visited):
+            visited.add(left)
+            queue.append(left)
+        if isChangedPixel(matrix, top, visited):
+            visited.add(top)
+            queue.append(top)
+        if isChangedPixel(matrix, bottom, visited):
+            visited.add(bottom)
+            queue.append(bottom)
+        #// queue.append((pixel_x + DFS_SKIP, pixel_y))
+        #// queue.append((pixel_x - DFS_SKIP, pixel_y))
+        #// queue.append((pixel_x, pixel_y + DFS_SKIP))
+        #// queue.append((pixel_x, pixel_y - DFS_SKIP))
 
         
-    top_left[0] -= 20
-    top_left[1] -= 20
-    bottom_right[0] += 20
-    bottom_right[1] += 20
     if count > ISLAND_SIZE:
-        cv2.rectangle(og, top_left, bottom_right, (0, 0, 255), thickness=1, lineType=cv2.LINE_8)  
-        print(f"found change in frame {frame_num} at ({x}, {y}) with island size: {count} ")
+        # Expand the rectangle 20 pixels every direction 
+        top_left[0] -= 20
+        top_left[1] -= 20
+        bottom_right[0] += 20
+        bottom_right[1] += 20
+        cv2.rectangle(original_image, top_left, bottom_right, COLOR, thickness=1, lineType=cv2.LINE_8)  
+        print(f"found change in frame {frame_number} at ({x}, {y}) with island size: {count} ")
         # send_message(f"found change in frame {frame_num} at ({x}, {y}) with island size: {count} ")
         # annot_img(og, x, y, str(count))
 
@@ -76,10 +107,11 @@ def annot_img(image, x, y, text):
     # cv2.waitKey(0)
 
 
-def process_image(compare, img, count, og, label: str):
+def process_image(comparer_image, img, count, original_image, label: str):
+    # Converts to int16 to prevent overflow (uint8 > 255 = -)
     img = img.astype('int16')
-    compare = compare.astype('int16')
-    change = compare - img
+    comparer_image = comparer_image.astype('int16')
+    change = comparer_image - img
     change = np.abs(change)
 
     visited = set()
@@ -88,39 +120,42 @@ def process_image(compare, img, count, og, label: str):
     # DFS
     for y in range(0, len(change), GRID):  # Optimized 
         for x in range(0, len(change[0]), GRID): # Optimized
-            total_changed_pixels += dfs(change, x, y, visited, og, frame_num=count)
+            total_changed_pixels += dfs(change, x, y, visited, original_image, frame_number=count)
 
     if total_changed_pixels >= ISLAND_SIZE:
-        annot_img(og, 30, 30, f"{total_changed_pixels} pixels displaced")
+        annot_img(original_image, 30, 30, f"{total_changed_pixels} pixels displaced")
         path = f'security/trigger/{label}'
         if not os.path.exists(path):
             os.mkdir(path)
-        cv2.imwrite(f"{path}/frame_{count}_{total_changed_pixels}.jpg", og)     # save frame as JPEG file      
-        # send_message(f":alarm_clock: MOTION DETECTED of size {total_changed_pixels} px! :alarm_clock:")
-    # cv2.imwrite(f"change/frame_{count}.jpg", change)     # save frame as JPEG file      
+        cv2.imwrite(f"{path}/frame_{count}_{total_changed_pixels}.jpg", original_image)     # save frame as JPEG file      
 
-def read_vid(video_label: str):
+def read_vid(video_label: str) -> None:
     vidcap = cv2.VideoCapture(f"security/video/{video_label}.mp4")
     success,image = vidcap.read()
-    base = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-    # cv2.imwrite(f"security/trigger/{video_label}/base.jpg", base)     # save frame as JPEG file      
+    # Black and White Image 
+    comparer_image: cv2.Mat  = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
     frame_count = 0
     while success:
-        img = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-        # Optimization Only
+        img: cv2.Mat = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+        # Optimization Only | compares first and last frame 
         if frame_count % FRAME_SKIP == FRAME_SKIP - 1:
-            process_image(compare=base, img=img, count=frame_count, og=image, label=video_label)
-        if frame_count % FRAME_SKIP == 0:
+            process_image(
+                comparer_image=comparer_image, 
+                img=img, count=frame_count, 
+                original_image=image, 
+                label=video_label
+            )
+        elif frame_count % FRAME_SKIP == 0:
             # Reset the comparer image 
-            base = img
+            comparer_image = img
 
         success,image = vidcap.read()
         frame_count += 1
 
-    print("processed job")
+    print(f'processed {video_label}')
 
-read_vid('0_1670559006.752842')
+read_vid("4_1670565479.302986")
 # def main():
 #     for filename in os.listdir('security/video'):
 #         # BAD 
--- jeny.py
+++ jeny.py
@@ -7,10 +7,11 @@ import shutil
 
 load_dotenv()
 TOKEN: str | None = os.getenv("DISCORD_TOKEN")
-CHANNEL_ID: int = 988215724680052757
-ID: str | None = os.getenv("DISCORD_ID")
-# CHANNEL_ID: int = 988215725367885876
+BOT_ID: str | None = os.getenv("DISCORD_ID")
+#// CHANNEL_ID: int = 988215724680052757
+#// CHANNEL_ID: int = 988215725367885876
 
+# Set up the client 
 intents = discord.Intents.default()
 client = discord.Client(intents=intents)
 
@@ -19,56 +20,75 @@ async def on_ready():
     print(f"{client.user} has connected!")
     
 
+# GLOBAL variable to handle the process
 pro: subprocess.Popen | None = None
 @client.event
 async def on_message(message):
-    global pro
+    global pro # Allows control of process 
     channel = message.channel
-    if str(message.author.id) == ID:
+    # Do not trigger when bot sends message
+    if str(message.author.id) == BOT_ID:
         return
+    # Only when the bot is referenced in the discord group
     if (len(message.content) <= 22):
         return
 
-    msg = message.content[22:].strip()
+    # Removes the <@BOT-ID> tag in the message 
+    msg: str = message.content[22:].strip()
 
     if (msg == "status"):
-        clips = set(os.listdir('security/trigger'))
-        if '.DS_Store' in clips:
-            clips.remove('.DS_Store')
+        # Sorts the clips in ascending order then checks if macs config file is there to remove it 
+        clips = sorted(os.listdir('security/trigger'))
+        if clips[0] == '.DS_Store':
+            clips = clips[1:]
 
+        # Empty clips then nothing to report 
         if len(clips) == 0:
             await channel.send("All Good")
             return
 
 
+        # Send how many clips exist 
         await channel.send(f"You have {len(clips)} clips")
         
         for clip in clips:
-            frames = os.listdir(f'security/trigger/{clip}')
-            if ".DS_Store" in frames:
-                frames.remove(".DS_Store")
+            frames = sorted(os.listdir(f'security/trigger/{clip}'))
+            if frames[0] == '.DS_Store':
+                frames = frames[1:]
+
             await channel.send(f"Fetching random frame per clip")
 
+            # Fetches random frame to show for each clip 
             choice = random.choice(frames)
 
+            # Send the image to the discord group 
             await channel.send(
                 file=discord.File(f"security/trigger/{clip}/{choice}")
             )
+    # Arms security system 
     elif (msg == 'arm'):
         await channel.send("Armed for 30 minutes, but I'm not counting.")
         # 30 minutes of footage 15 second clips (120 clips )
+        # Runs in a seperate process and saves the details to the global variable pro to then be able to kill it 
         pro = subprocess.Popen(
             "python3.10 arm.py --wait 0 --spv 15 --vid_num 120", shell=True, start_new_session=True, stdout=subprocess.PIPE)
     
+    # Clears clips and frames 
     elif (msg == 'clear'):
         clips = os.listdir('security/trigger')
+        videos = os.listdir('security/video')
         for clip in clips:
             if clip == '.DS_Store':
                 continue
             shutil.rmtree(f'security/trigger/{clip}', ignore_errors=False, onerror=None)
+        for video in videos:
+            if video == '.DS_Store':
+                continue
+            os.remove(f'security/video/{video}')
 
         await channel.send("Cleared Clips")
 
+    # Kills the arm process then sets to None for no process running 
     elif (msg == 'disarm'):
         if not pro:
             await channel.send("Nothing was armed in the first place ")
@@ -76,9 +96,11 @@ async def on_message(message):
         await channel.send(f"Disarming process: {pro.pid}")
         pro.kill()
         await channel.send("Disarmed")
+        pro = None
+
+    # Kills jeny please do not kill her  
     elif (msg == 'suicide'):
         await channel.send("*Jeny fell off a tall bridge*")
         exit()
-    # await channel.send("on message")
 
 client.run(str(TOKEN))
